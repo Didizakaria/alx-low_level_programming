5a1cc4ecf1f0bef4fda15798885512952be212aa
Didizakaria <dev.didizakaria@gmail.com>
2023-03-16 15:19:17 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<unistd.h>
+
 /**
  * main -Enter point
  *
@@ -8,5 +9,5 @@ int main(void)
 {
 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	write(2, message, 39);
-	return 1;
+	return (1);
 }
