ef21f4cd18018af71feab9eefb9d24be309c3f42
Didizakaria <dev.didizakaria@gmail.com>
2023-03-16 15:14:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-	const char* msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	const int len = 54;
-	const int err = 2;
-	(void) fwrite(err, msg, len);
-	return (1);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, message, 39);
+	return 1;
 }
