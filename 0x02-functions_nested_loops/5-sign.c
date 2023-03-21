aa5bba0342bf28877c855a96f254a874f1641f5e
Didizakaria <dev.didizakaria@gmail.com>
2023-03-21 14:50:14 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
+ * print_sign - determine print if a number is positive,negative or equal to 0
  *
- * @n: character in ASCII
+ * @n: is an integer
  *
- * return :1 and prints + if n is greater than zero 0 and prints 0 if n is zero , -1 and prints - if n is less than zero
+ * return :1 and prints + if n is greater than zero 0
+ * and prints 0 if n is zero
+ * -1 and prints if n is less than zero
  *
  */
 int print_sign(int n)
