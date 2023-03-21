89aefc7a5b43e58cd8f3c0b60492e01a72b8247c
Didizakaria <dev.didizakaria@gmail.com>
2023-03-21 13:07:59 -0700

    uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * _isalpha: checks for alphabetic character
+ *
+ * @c: character in ASCII
+ *
+ * Return: 1 if lowercase 0 if someting else
+ *
+ */
+int _isalpha(int c)
+{
+	
+	if ((i >= 65 && i<=90) || (i >= 97 && i<= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
