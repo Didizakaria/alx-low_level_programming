08ba8a0b3112f5d337a4861061d840b95052a26f
Didizakaria <dev.didizakaria@gmail.com>
2023-03-22 13:43:23 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,7 +5,8 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * retun: largest number
+ *
+ * Retrun: largest number
  */
 
 int largest_number(int a, int b, int c)
@@ -13,6 +14,7 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	largest = a;
+
 	if (b > largest)
 	{
 		largest = b;
