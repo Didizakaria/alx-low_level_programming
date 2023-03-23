961cf28cc90e0befdb704adc4bf9979f51f3e9a8
Didizakaria <dev.didizakaria@gmail.com>
2023-03-23 15:30:06 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - this fuction print a line
+ *
+ * @n:is a number
+ *
+ * return: no return
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(98);
+	}
+	_putchar('\n');
+}
