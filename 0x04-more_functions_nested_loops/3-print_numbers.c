872df31d2a8e009aaabcabf4ffe95e8581b381e7
Didizakaria <dev.didizakaria@gmail.com>
2023-03-23 09:00:56 -0700

    from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ *
+ * Return: 0 if successful
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	for (i <= 9,i++)
+	{
+	_putchar(i);
+	}
+
+	_putchar('\n');
+}
