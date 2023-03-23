3e1219fb1145ad005e34658b42af52f065eff18d
Didizakaria <dev.didizakaria@gmail.com>
2023-03-23 08:52:56 -0700

    multiplay

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: multiplication of two numbers
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = 0;
+	m = a * b;
+
+	return (m);
+}
