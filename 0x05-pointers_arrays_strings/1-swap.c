d6d5d83d7c54c47b821cc9480d075fdbc9c6f5b5
Didizakaria <dev.didizakaria@gmail.com>
2023-03-27 04:06:34 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * swap_int - swaps the values
+ *
+ * @a: first int
+ * @b: second int
+ * 
+ * Return : new values swaped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temps;
+}
