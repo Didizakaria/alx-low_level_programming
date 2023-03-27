0edb72c2f27afe98c272a5e41fbed55de7b2dca1
Didizakaria <dev.didizakaria@gmail.com>
2023-03-27 06:35:21 -0700

    corrections

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int count = 0;
 
-	while (*scr[i] != '\0')
+	while (count >= 0)
 	{
-		*dest[i] = *src[i];
-		i++;
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
 	}
-
-	*dest[i] = '\0';
-	return (*dest);
+	return (dest);
 }
