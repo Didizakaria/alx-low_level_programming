445f9daef9f55d53e513b9614c725fcb8afbebbf
Didizakaria <dev.didizakaria@gmail.com>
2023-03-27 05:22:49 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @s: parameter
+ *
+ * Return: string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i];
+	}
+	_puchar('\n');
+}
+
