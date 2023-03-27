899305ed61336b495a93d10e00c89d2158906e68
Didizakaria <dev.didizakaria@gmail.com>
2023-03-27 04:16:08 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * int _strlen - length of a string
+ *
+ * @s: parameter
+ *
+ * Return: returns the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
