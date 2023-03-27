b94ab3bfa462b369e8c542bd35e7095755fec8c1
Didizakaria <dev.didizakaria@gmail.com>
2023-03-27 06:24:50 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse
- *
- * @s: parameter
- *
- * Return: no return
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
  */
-
 void print_rev(char *s)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (count >= 0)
 	{
+		if (s[count] == '\0')
+			break;
+		count++;
 	}
 
-	for (i--; i >= 0; i--)
-	{
-		_putchar(s[i];
-	}
-	_puchar('\n');
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
-
