31a16733fc19003bb1e0dda17acde853e41868b6
Didizakaria <dev.didizakaria@gmail.com>
2023-04-03 06:00:01 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ *
+ * @*s: char
+ * @b: char
+ * @n: unsigned int
+ *
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return s;
+}
