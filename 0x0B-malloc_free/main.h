38b1539377bee0a85081a152a164a93911490ffe
Didizakaria <dev.didizakaria@gmail.com>
2023-04-11 10:01:13 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
 char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char **split_string(char *str, char delim);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-#endif
+#endif /* MAIN_H */
