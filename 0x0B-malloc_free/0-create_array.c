a40a1201ca0ee55e4b29497163033922d24c4dff
Didizakaria <dev.didizakaria@gmail.com>
2023-04-11 09:20:37 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
-	char *arr = malloc(size * sizeof(char));
-	
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
+	char *arr;
+	unsigned int;
 
-	for (unsigned int i = 0; i < size; i++)
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
