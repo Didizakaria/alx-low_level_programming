d40789ddcb2da4dbadeae4853cf4186900e5c4ea
Didizakaria <dev.didizakaria@gmail.com>
2023-04-17 16:42:54 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to new dog_t struct or NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (!new_dog)
+		return (NULL);
+
+	name_copy = strdup(name);
+	if (!name_copy)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	owner_copy = strdup(owner);
+	if (!owner_copy)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
+}
