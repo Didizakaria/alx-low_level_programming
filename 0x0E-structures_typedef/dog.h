7b5f24c09b8db3fe6c2674e87ef643c7eab082e8
Didizakaria <dev.didizakaria@gmail.com>
2023-04-17 16:07:38 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Structure for a dog's information
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: A structure that holds information about a dog
+ */
 struct dog
 {
 	char *name;
