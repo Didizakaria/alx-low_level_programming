e54c6329ae753bd1fca14cc2f46d2adb69f8c6a3
Didizakaria <dev.didizakaria@gmail.com>
2023-04-19 18:44:57 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,6 +11,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
@@ -19,9 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-		{
 			return (i);
-		}
 	}
 
 	return (-1);
