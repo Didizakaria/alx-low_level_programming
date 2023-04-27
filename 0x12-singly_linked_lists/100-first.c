bfb47a6ef1bcceb320ab2c6cc37d35238bc7b09e
Didizakaria <dev.didizakaria@gmail.com>
2023-04-27 09:46:56 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) before_main()
+void __attribute__((constructor)) calledBeforeMain();
+
+/**
+ * calledBeforeMain - function that is executed before main()
+ *
+ * Description: This function is executed before main() and it prints the
+ * message "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+ * using printf().
+ */
+void calledBeforeMain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
+
