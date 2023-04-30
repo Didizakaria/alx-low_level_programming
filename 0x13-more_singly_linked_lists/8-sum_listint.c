db3319245ec83f92a2adf2f8a24b919f790c0c1d
Didizakaria <dev.didizakaria@gmail.com>
2023-04-30 09:33:05 -0700

    8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - returns the sum of all the data (n) of a linked list
  * @head: pointer to the head of the linked list
  *
- * Return: sum of all the data (n) of the linked list, or 0 if the list is empty
+ * Return: sum of the data (n) of the linked list, or 0 if list is empty
  */
 int sum_listint(listint_t *head)
 {
