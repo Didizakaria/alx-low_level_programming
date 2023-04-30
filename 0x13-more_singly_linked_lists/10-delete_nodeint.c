e0e59d7e9f82a56ff7e16ba6b86904c8e8645821
Didizakaria <dev.didizakaria@gmail.com>
2023-04-30 09:48:03 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes the node at index index
  * @head: pointer to the head node of the list
  * @index: index of the node to delete
  *
