69d72b1ede231858a2b55453ca9a57a2c73edc42
Didizakaria <dev.didizakaria@gmail.com>
2023-05-01 01:40:19 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,41 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
+#include <stddef.h>
+
 /**
- * print_listint_safe - prints a listint_t linked list
- * @head: pointer to the head node of the list
+ * print_listint_safe - Prints all the elements of a listint_t list
+ * @head: Pointer to the head node of the list
  *
- * Return: number of nodes in the list
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *slow = head, *fast = head;
+	const listint_t *current = head, *temp = NULL;
 
-	if (head == NULL)
-		exit(98);
-
-	while (fast && fast->next)
+	while (current != NULL)
 	{
 		count++;
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
+		printf("[%p] %d\n", (void *) current, current->n);
 
-		if (slow == fast)
+		if (current > current->next || temp > current->next)
 		{
-			count++;
-			printf("[%p] %d\n", (void *)slow, slow->n);
+			printf("-> [%p] %d\n", (void *) current->next, current->next->n);
 			break;
 		}
+
+		temp = current;
+		current = current->next;
 	}
 
-	if (!fast || !fast->next)
-	{
-		count++;
-		printf("[%p] %d\n", (void *)slow, slow->n);
-	}
-
-	return count;
+	return (count);
 }
