a17b07684dca47135a20946e323ba55688b6d234
Didizakaria <dev.didizakaria@gmail.com>
2023-05-01 01:34:22 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,50 +1,41 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * print_listint_safe - prints a listint_t linked list
- * @head: pointer to the head of the list
+ * @head: pointer to the head node of the list
  *
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise = head, *hare = head;
 	size_t count = 0;
+	const listint_t *slow = head, *fast = head;
 
-	while (hare != NULL && hare->next != NULL)
+	if (head == NULL)
+		exit(98);
+
+	while (fast && fast->next)
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		count++;
-		tortoise = tortoise->next;
-		hare = hare->next->next;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
 
-		/* Check for cycle */
-		if (tortoise == hare)
+		if (slow == fast)
 		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 			count++;
+			printf("[%p] %d\n", (void *)slow, slow->n);
 			break;
 		}
 	}
 
-	/* No cycle found, print remaining nodes */
-	if (hare == NULL || hare->next == NULL)
+	if (!fast || !fast->next)
 	{
-		while (tortoise != NULL)
-		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
-		}
+		count++;
+		printf("[%p] %d\n", (void *)slow, slow->n);
 	}
 
-	/* Check if function failed */
-	if (count == 0)
-	{
-		exit(98);
-	}
-
-	return (count);
+	return count;
 }
