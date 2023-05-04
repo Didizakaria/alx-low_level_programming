3b74bba8a36e9b68b975356270d3a452442dbd8f
Didizakaria <dev.didizakaria@gmail.com>
2023-05-04 10:07:15 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,13 +11,21 @@ void print_binary(unsigned long int n)
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 	int started = 0;
 
-	while (mask > 0) {
+	while (mask > 0)
+	{
 		if (n & mask)
-			started = 1, _putchar('1');
+		{
+			started = 1;
+			_putchar('1');
+		}
 		else if (started)
+		{
 			_putchar('0');
+		}
 		mask >>= 1;
 	}
 	if (!started)
+	{
 		_putchar('0');
+	}
 }
